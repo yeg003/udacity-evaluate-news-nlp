eb1c54f9ef79d039c8f810fec34e5e0a598c1372
Yancarlos <yeg003@gmail.com>
2020-04-14 14:48:06 -0400

    added files and loaders for sass

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,12 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
+import './styles/resets.scss'
+import './styles/header.scss'
+import './styles/form.scss'
+import './styles/footer.scss'
+import './styles/base.scss'
+
 console.log(checkForName);
 
 alert("I EXIST")
